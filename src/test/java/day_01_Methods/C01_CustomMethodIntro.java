db33789291729e63cdba3ebeb995d9b2332f01a3
beytio <oguzbeytullah@gmail.com>
2023-04-28 15:41:28 +0300

    first com

--- src/test/java/day_01_Methods/C01_CustomMethodIntro.java
+++ src/test/java/day_01_Methods/C01_CustomMethodIntro.java
@@ -3,6 +3,8 @@ package day_01_Methods;
 public class C01_CustomMethodIntro {
 
     public static void main(String[] args) {
+
+        // bilal bey merhaba kaç giriş yapmaya çalıştınız:)
         //System.out.println("Hello world");
         System.out.println("Program started");
         sayHello();
